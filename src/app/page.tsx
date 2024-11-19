36b0bb9c957292007a9d7ce794053ae0681a16f9
Guilherme Augusto montalvão <guilherme06075828125@gmail.com>
2024-11-19 18:21:31 -0300

    Piloto

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import Render from "@/components/render";
+import Image from "next/image";
+import { Canvas } from "@react-three/fiber"
+
+export default function Home() {
+  
+  return (
+    <div className="flex w-full overflow-x-hidden flex-col">
+      {/* Página 1 */}
+      <div className="w-full overflow-x-hidden h-screen relative flex">
+        <Image
+          src={'/homepage-bg.png'}
+          alt="Homepage image"
+          fill
+          className="object-cover z-0 pointer-events-none"
+        />
+        <div className="flex flex-1 flex-col z-[1] justify-center items-center text-center">
+          <h1 className="text-[54px] font-bold">BEM-VINDO A WELBERCARS</h1>
+          <h3 className="text-[24px]">SUA REVENDEDORA MCLAREN</h3>
+        </div>
+      </div>
+      {/* Página 2 */}
+      <div className="w-full overflow-x-hidden h-screen flex justify-center items-center">
+        <Canvas
+          className='h-full w-full'
+          shadows
+          camera={{
+            position: [-6, 2, 2],
+          }}
+        >
+          <Render modelPath='/2022-mclaren-765lt/Final_Model.fbx' />
+        </Canvas>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/bars/NavBar.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export function NavBar() {
+    return (
+        <div className="absolute flex flex-row z-50 w-screen h-[131px] justify-between bg-black px-6">
+            <Image
+              src={'/logo.png'}
+              alt="logo"
+              width={385}
+              height={81}
+            />
+            <div className="flex flex-row justify-center items-center gap-5">
+                <Link href={'/about'} className="text-xl">Sobre</Link>
+                <Link href={'/about'} className="text-xl">Sobre</Link>
+                <Link href={'/about'} className="text-xl">Sobre</Link>
+                <Link href={'/about'} className="text-xl">Sobre</Link>
+                <button className="text-xl border p-2 border-white">Register Now</button>
+            </div>
+        </div>
+    )
+}
